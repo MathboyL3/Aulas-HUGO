25553d97d3bf36f05096504936a04b60d7cc00a9
MathboyL3 <63455990+MathboyL3@users.noreply.github.com>
2025-03-17 21:49:02 -0300

    buasca

---
+++ BuscaPorIndex/Program.cs
@@ -0,0 +1,39 @@
+﻿struct item
+{
+    public int chave;
+    public int dado;
+
+    public item(int chave, int dado)
+    {
+        this.chave = chave;
+        this.dado = dado;
+    }
+}
+
+public static class Program
+{
+    public static void Main()
+    {
+        int n = 1000;
+        List<item> lista = new();
+
+        for (int i = 0; i < n; i++)
+            lista.Add(new item(Random.Shared.Next(), Random.Shared.Next()));
+
+        Console.WriteLine($"O item foi encontrado no index {Busca(lista, lista[n-1].chave, n)}");
+    }
+
+    static int Busca(IList<item> lista, int chave, int tamanhoLista)
+    {
+        int i = 0;
+        while(i < tamanhoLista)
+        {
+            if (lista[i].chave == chave)
+                return i;
+            i += 1;
+        }
+        return -1;
+    }
+}
+
+
---
+++ MaxVec/Program.cs
@@ -0,0 +1,57 @@
+﻿
+int[] numbers = { 8, 5, 2, 9 };
+
+Console.WriteLine($" Antes: {string.Join(", ", numbers)}");
+Order(numbers);
+Console.WriteLine($"Depois: {string.Join(", ", numbers)}");
+
+int Max(int[] vec, int from, int to)
+{
+    var max = numbers[from];
+    var idx = from;
+    for (int i = from; i <= to; i++)
+    {
+        if (numbers[i] > max)
+        {
+            max = numbers[i];
+            idx = i;
+        }
+    }
+    return idx;
+}
+
+int Min(int[] vec, int from, int to)
+{
+    var min = numbers[from];
+    var idx = from;
+    for (int i = from; i <= to; i++)
+    {
+        if (numbers[i] < min)
+        {
+            min = numbers[i];
+            idx = i;
+        }
+    }
+    return idx;
+}
+
+void Order(int[] arr)
+{
+    int maxB = arr.Length - 1;
+    int n = arr.Length / 2;
+
+    if (arr.Length % 2 == 0)
+        n += 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        var lastPos = maxB - i;
+        var min = Min(arr, i, lastPos);
+        var max = Max(arr, i, lastPos);
+
+        if (min == max) break;
+
+        (arr[i], arr[min]) = (arr[min], arr[i]);
+        (arr[lastPos], arr[max]) = (arr[max], arr[lastPos]);
+    }
+}
